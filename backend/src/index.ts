c14fa44641ebf13ee2dfe1151fe920524d9b5b39
Danil Zharkov <zharkov.danik.ua@gmail.com>
2025-08-30 23:40:31 -0600

    feature/get-page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,11 @@ import {
 const PORT = 4000
 
 const server = createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  
   if (req.url === '/quotes' && req.method === 'GET') {
     return getQuotesController(req, res)
   }
---
+++ frontend/src/app/get-quotes/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+type Quote = {
+  name?: string
+  desc?: string
+}
+
+const GetQuotesPage: React.FC = () => {
+  const [quotes, setQuotes] = useState([])
+
+  const getQuotes = async () => {
+    try {
+      const res = await axios.get('http://localhost:4000/quotes')
+      const data = res.data
+      setQuotes(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getQuotes()
+  }, [])
+
+  return (
+    <div>
+      {quotes.length > 0 &&
+        quotes.map((q: Quote, i) => {
+          return <div className='text-white' key={i}>{q.name}</div>
+        })}
+    </div>
+  )
+}
+
+export default GetQuotesPage
